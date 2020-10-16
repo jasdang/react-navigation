5a429ece9036c1eb04555fc9ebf8b858867b89fb
Jasmine Dang <ngandth.1992@gmail.com>
2020-10-16 22:32:52 +0800

    create stack navigation, home screen and details screen

--- App.js
+++ App.js
@@ -2,17 +2,36 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen name='HomeScreen' component={HomeScreen} options={{ title: 'Home Screen' }}/>
+        <Stack.Screen name='DetailsScreen' component={DetailsScreen} options={{ title: 'Details Screen' }}/>
+      </Stack.Navigator>
     </NavigationContainer>
   );
+}
+
+export function HomeScreen() {
+  return (
+    <View>
+      <Text>This is Home Screen</Text>
+    </View>
+  )
+}
+
+export function DetailsScreen() {
+  return (
+    <View>
+      <Text>This is Details Screen</Text>
+    </View>
+    )
 }
 
 const styles = StyleSheet.create({
