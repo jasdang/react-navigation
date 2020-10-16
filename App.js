b8d8ea37dd398bf458253fcd2c78b16436c227c3
Jasmine Dang <ngandth.1992@gmail.com>
2020-10-16 22:43:26 +0800

    moving between screens

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 const Stack = createStackNavigator();
 
@@ -11,25 +11,32 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='HomeScreen' component={HomeScreen} options={{ title: 'Home Screen' }}/>
-        <Stack.Screen name='DetailsScreen' component={DetailsScreen} options={{ title: 'Details Screen' }}/>
+        <Stack.Screen name='Home' component={HomeScreen} options={{ title: 'Home Screen' }}/>
+        <Stack.Screen name='Details' component={DetailsScreen} options={{ title: 'Details Screen' }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-export function HomeScreen() {
+export function HomeScreen({ navigation }) {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>This is Home Screen</Text>
+      <Button onPress={() => navigation.navigate('Details')} title='Go to Details Screen'></Button>
+      <Button onPress={() => navigation.goBack()} title='Go back'></Button>
+
     </View>
   )
 }
 
-export function DetailsScreen() {
+export function DetailsScreen({ navigation }) {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>This is Details Screen</Text>
+      <Button onPress={() => navigation.push('Details')} title='Go down to Another Details Screen'></Button>
+      <Button onPress={() => navigation.push('Home')} title='Go down to Home Screen'></Button>
+      <Button onPress={() => navigation.popToTop('Details')} title='Jump back to Home Screen'></Button>
+      <Button onPress={() => navigation.goBack()} title='Go back'></Button>
     </View>
     )
 }
