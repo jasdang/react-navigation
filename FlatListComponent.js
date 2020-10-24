f858526cabc8dbcf0aefdf0b1defbc5d5ada0d62
Jasmine Dang <ngandth.1992@gmail.com>
2020-10-24 17:00:33 +0800

    render FlatListComponent

--- FlatListComponent.js
+++ FlatListComponent.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import {FlatList, View, Text, StyleSheet} from 'react-native';
 
+const items = [
+  {id: '0', text: 'View'},
+  {id: '1', text: 'Text'},
+  {id: '2', text: 'Image'},
+  {id: '3', text: 'ScrollView'},
+  {id: '4', text: 'ListView'},
+];
+
 export default function FlatListComponent() {
   return (
-    <View style={styles.container}>
-      <Text>FlatList</Text>
-    </View>
+    <FlatList
+      style={styles.container}
+      data={items}
+      renderItem={({item}) => <Text style={styles.row}>{item.text}</Text>}
+      keyExtractor={(item) => item.id}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: 30,
+  },
+  row: {
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: 'skyblue',
   },
 });
